5170cba446636100ab267e74191afe8e429abe7d
sam marshall <s.marshall@open.ac.uk>
2021-04-12 18:11:48 +0100

    VLE in the cloud: Complete data masking system (equivalent to learn2) #459423

---
+++ classes/tool_datamasking.php
@@ -0,0 +1,50 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace mod_openstudio;
+
+/**
+ * Implementation of data masking for this plugin.
+ *
+ * The corresponding test script tool_datamasking_test.php checks every masked field.
+ *
+ * @package mod_openstudio
+ * @copyright 2021 The Open University
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class tool_datamasking implements \tool_datamasking\plugin {
+
+    public function build_plan(\tool_datamasking\plan $plan): void {
+        $plan->table('files')->add(
+                new \tool_datamasking\files_mask('mod_openstudio', 'content'));
+        $plan->table('files')->add(
+                new \tool_datamasking\files_mask('mod_openstudio', 'contentcomment'));
+        $plan->table('files')->add(
+                new \tool_datamasking\files_mask('mod_openstudio', 'contenttemp'));
+        $plan->table('files')->add(
+                new \tool_datamasking\files_mask('mod_openstudio', 'contentthumbnail'));
+        $plan->table('files')->add(
+                new \tool_datamasking\files_mask('mod_openstudio', 'notebook'));
+
+        $plan->table('openstudio')->add(new \tool_datamasking\unique_email_mask('reportingemail'));
+        $plan->table('openstudio_comments')->add(new \tool_datamasking\similar_text_mask(
+                'title', false, \tool_datamasking\similar_text_mask::MODEL_SUBJECT));
+        $plan->table('openstudio_comments')->add(new \tool_datamasking\similar_text_mask(
+                'commenttext', true, \tool_datamasking\similar_text_mask::MODEL_POST));
+        $plan->table('openstudio_contents')->add(new \tool_datamasking\fixed_value_mask(
+                'ownershipdetail', 'Masked content'));
+    }
+}
---
+++ tests/tool_datamasking_test.php
@@ -0,0 +1,100 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace mod_openstudio;
+
+/**
+ * Tests the tool_datamasking class for this plugin.
+ *
+ * @package mod_openstudio
+ * @copyright 2021 The Open University
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class tool_datamasking_test extends \advanced_testcase {
+
+    /**
+     * Tests actual behaviour of the masking applied in this plugin.
+     */
+    public function test_behaviour(): void {
+        global $DB;
+
+        $this->resetAfterTest();
+
+        // Set up data to be masked.
+        $openstudioid1 = $DB->insert_record('openstudio', ['course' => 0, 'name' => '',
+                'timemodified' => 0, 'reportingemail' => 'secret@example.org']);
+        $DB->insert_record('openstudio', ['course' => 0, 'name' => '', 'timemodified' => 0]);
+        $DB->insert_record('openstudio_comments', ['contentid' => 0, 'userid' => 0,
+                'timemodified' => 0, 'title' => 'Q', 'commenttext' => 'Q.']);
+        $DB->insert_record('openstudio_comments', ['contentid' => 0, 'userid' => 0,
+                'timemodified' => 0, 'commenttext' => '']);
+        $DB->insert_record('openstudio_contents', ['openstudioid' => 0, 'levelid' => 0,
+                'levelcontainer' => 0, 'userid' => 0, 'timemodified' => 0,
+                'ownershipdetail' => 'Secret']);
+        $DB->insert_record('openstudio_contents', ['openstudioid' => 0, 'levelid' => 0,
+                'levelcontainer' => 0, 'userid' => 0, 'timemodified' => 0]);
+
+        // Add some files.
+        $fileids = [];
+        $fileids[] = \tool_datamasking\testing_utils::add_file('mod_openstudio', 'content',
+                'a.txt', 'a');
+        $fileids[] = \tool_datamasking\testing_utils::add_file('mod_openstudio', 'contentcomment',
+                'b.txt', 'bb');
+        $fileids[] = \tool_datamasking\testing_utils::add_file('mod_openstudio', 'contenttemp',
+                'c.txt', 'ccc');
+        $fileids[] = \tool_datamasking\testing_utils::add_file('mod_openstudio', 'contentthumbnail',
+                'd.txt', 'dddd');
+        $fileids[] = \tool_datamasking\testing_utils::add_file('mod_openstudio', 'notebook',
+                'e.txt', 'eeeee');
+        $fileids[] = \tool_datamasking\testing_utils::add_file('mod_openstudio', 'intro',
+                'f.txt', 'ffffff');
+
+        // Before checks.
+        $openstudiosql = 'SELECT reportingemail FROM {openstudio} ORDER BY id';
+        $this->assertEquals(['secret@example.org', null], $DB->get_fieldset_sql($openstudiosql));
+        $openstudiocommentstitlesql = 'SELECT title FROM {openstudio_comments} ORDER BY id';
+        $this->assertEquals(['Q', null], $DB->get_fieldset_sql($openstudiocommentstitlesql));
+        $openstudiocommentscommenttextsql = 'SELECT commenttext FROM {openstudio_comments} ORDER BY id';
+        $this->assertEquals(['Q.', ''], $DB->get_fieldset_sql($openstudiocommentscommenttextsql));
+        $openstudiocontentssql = 'SELECT ownershipdetail FROM {openstudio_contents} ORDER BY id';
+        $this->assertEquals(['Secret', ''], $DB->get_fieldset_sql($openstudiocontentssql));
+
+        \tool_datamasking\testing_utils::check_file($this, $fileids[0], 'a.txt', 1);
+        \tool_datamasking\testing_utils::check_file($this, $fileids[1], 'b.txt', 2);
+        \tool_datamasking\testing_utils::check_file($this, $fileids[2], 'c.txt', 3);
+        \tool_datamasking\testing_utils::check_file($this, $fileids[3], 'd.txt', 4);
+        \tool_datamasking\testing_utils::check_file($this, $fileids[4], 'e.txt', 5);
+        \tool_datamasking\testing_utils::check_file($this, $fileids[5], 'f.txt', 6);
+
+        // Run the full masking plan including this plugin.
+        \tool_datamasking\api::get_plan()->execute();
+
+        // After checks.
+        $this->assertEquals(['email' . $openstudioid1 . '@open.ac.uk.invalid', null],
+                $DB->get_fieldset_sql($openstudiosql));
+        $this->assertEquals(['X', null], $DB->get_fieldset_sql($openstudiocommentstitlesql));
+        $openstudiocommentscommenttextsql = 'SELECT commenttext FROM {openstudio_comments} ORDER BY id';
+        $this->assertEquals(['X.', ''], $DB->get_fieldset_sql($openstudiocommentscommenttextsql));
+        $this->assertEquals(['Masked content', ''], $DB->get_fieldset_sql($openstudiocontentssql));
+
+        \tool_datamasking\testing_utils::check_file($this, $fileids[0], 'masked.txt', 224);
+        \tool_datamasking\testing_utils::check_file($this, $fileids[1], 'masked.txt', 224);
+        \tool_datamasking\testing_utils::check_file($this, $fileids[2], 'masked.txt', 224);
+        \tool_datamasking\testing_utils::check_file($this, $fileids[3], 'masked.txt', 224);
+        \tool_datamasking\testing_utils::check_file($this, $fileids[4], 'masked.txt', 224);
+        \tool_datamasking\testing_utils::check_file($this, $fileids[5], 'f.txt', 6);
+    }
+}
